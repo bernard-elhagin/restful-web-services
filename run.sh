2b66cba42150b981543cd70626e25411c33d05fb
Bernard El-Hagin <bernard.elhagin@gmail.com>
2022-12-12 13:31:10 +0100

    Added two REST APIs

---
+++ run.sh
@@ -0,0 +1,3 @@
+#/usr/bin/sh
+
+/usr/lib/eclipse/plugins/org.eclipse.justj.openjdk.hotspot.jre.full.linux.x86_64_17.0.5.v20221102-0933/jre/bin/java -Dfile.encoding=UTF-8 -jar target/restful-web-services-0.0.1-SNAPSHOT.jar
