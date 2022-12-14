812ae2f5f1804939b467d8f7231dad273bc2f015
Bernard El-Hagin <bernard.elhagin@gmail.com>
2022-12-14 14:42:14 +0100

    Added universal REST resource link

--- src/main/java/com/tutorial/rest/webservices/restfulwebservices/helloworld/HelloWorldController.java
+++ src/main/java/com/tutorial/rest/webservices/restfulwebservices/helloworld/HelloWorldController.java
@@ -1,27 +1,17 @@
 package com.tutorial.rest.webservices.restfulwebservices.helloworld;
 
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
+
+import jakarta.servlet.http.HttpServletRequest;
 
 @RestController
 public class HelloWorldController {
 
-    @GetMapping(path = "/hello-world")
-    public String helloWorld() {
-        return "Hello World";
-    }
-
-    @GetMapping(path = "/hello-world-bean")
-    public HelloWorldBean helloWorldBean() {
-        return new HelloWorldBean("Hello World (bean)");
-    }
-
-    @GetMapping(path = "/hello-world/path-variable/{name}")
-    public HelloWorldBean helloWorldPathVariable(@PathVariable String name) {
-        return new HelloWorldBean(
-            String.format("Hello, %s", name)
-        );
+    @GetMapping(path = "/**")
+    public String helloWorld(HttpServletRequest req) {
+        String link = req.getRequestURL().toString();
+        return link;
     }
 
 }
