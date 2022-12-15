602ce1d339e0f75788f36ce93147ef96ef5f0bee
Bernard El-Hagin <bernard.elhagin@gmail.com>
2022-12-15 13:29:19 +0100

    Added basic authorization

--- src/main/java/com/tutorial/rest/webservices/restfulwebservices/helloworld/HelloWorldController.java
+++ src/main/java/com/tutorial/rest/webservices/restfulwebservices/helloworld/HelloWorldController.java
@@ -1,7 +1,14 @@
 package com.tutorial.rest.webservices.restfulwebservices.helloworld;
 
+import java.util.Arrays;
+
+import org.apache.tomcat.util.codec.binary.Base64;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -15,20 +22,38 @@ public class HelloWorldController {
     @Value("${clarity.global.url}")
     private String clarityUrl;
 
+    @Value("${clarity.user}")
+    private String clarityUser;
+
+    @Value("${clarity.password}")
+    private String clarityPassword;
+
+//    @GetMapping(path = "/**")
+//    public ResponseEntity<String> helloWorld(HttpServletRequest req) {
+//        return new ResponseEntity<>("Hello World!", HttpStatus.OK);
+//    }
+//
     @GetMapping(path = "/**")
-    public ResponseEntity<String> helloWorld(HttpServletRequest req) {
-        return new ResponseEntity<>("Hello World!", HttpStatus.OK);
+    public ResponseEntity<String> helloWorldTest(HttpServletRequest req) {
+
+        String url = clarityUrl + req.getRequestURI();
+        ResponseEntity<String> resp = new RestTemplate().exchange(url, HttpMethod.GET, new HttpEntity<>(httpHeaders()), String.class, HttpStatus.ACCEPTED);
+
+        return resp;
     }
 
-    @GetMapping(path = "/test")
-    public String helloWorldTest(HttpServletRequest req) {
+    private HttpHeaders httpHeaders() {
+        HttpHeaders httpHeaders = new HttpHeaders();
+        httpHeaders.add("Authorization", "Basic " + getBasicAuthHeader());
+        //httpHeaders.setAccept(Arrays.asList(MediaType.APPLICATION_JSON));
 
-        String url = clarityUrl + "/test";
-        RestTemplate restTemplate = new RestTemplate();
+        return httpHeaders;
+    }
 
-        String result = restTemplate.getForObject(url, String.class);
+    private String getBasicAuthHeader() {
+        String credentials = clarityUser + ":" + clarityPassword;
 
-        return result;
+        return new String(Base64.encodeBase64String(credentials.getBytes()));
     }
 
 }
