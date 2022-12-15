06a3149c9a60717dd6383a5c981da175d7c48f2f
Bernard El-Hagin <bernard.elhagin@gmail.com>
2022-12-15 09:35:29 +0100

    Added call to outside service

--- src/main/java/com/tutorial/rest/webservices/restfulwebservices/helloworld/HelloWorldController.java
+++ src/main/java/com/tutorial/rest/webservices/restfulwebservices/helloworld/HelloWorldController.java
@@ -1,18 +1,34 @@
 package com.tutorial.rest.webservices.restfulwebservices.helloworld;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
 import jakarta.servlet.http.HttpServletRequest;
 
 @RestController
 public class HelloWorldController {
 
+    @Value("${clarity.global.url}")
+    private String clarityUrl;
+
     @GetMapping(path = "/**")
-    public String helloWorld(HttpServletRequest req) {
-        String link = req.getRequestURL().toString();
-        return link;
+    public ResponseEntity<String> helloWorld(HttpServletRequest req) {
+        return new ResponseEntity<>("Hello World!", HttpStatus.OK);
+    }
+
+    @GetMapping(path = "/test")
+    public String helloWorldTest(HttpServletRequest req) {
+
+        String url = clarityUrl + "/test";
+        RestTemplate restTemplate = new RestTemplate();
+
+        String result = restTemplate.getForObject(url, String.class);
+
+        return result;
     }
 
 }
-
