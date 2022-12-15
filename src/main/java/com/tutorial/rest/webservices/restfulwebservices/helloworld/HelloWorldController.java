33c82d931785e6bcb00d47d35bc09ba811d64b85
Bernard El-Hagin <bernard.elhagin@gmail.com>
2022-12-15 21:25:30 +0100

    Added POST to base controller

--- src/main/java/com/tutorial/rest/webservices/restfulwebservices/helloworld/HelloWorldController.java
+++ src/main/java/com/tutorial/rest/webservices/restfulwebservices/helloworld/HelloWorldController.java
@@ -1,16 +1,14 @@
 package com.tutorial.rest.webservices.restfulwebservices.helloworld;
 
-import java.util.Arrays;
-
 import org.apache.tomcat.util.codec.binary.Base64;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
@@ -28,16 +26,24 @@ public class HelloWorldController {
     @Value("${clarity.password}")
     private String clarityPassword;
 
-//    @GetMapping(path = "/**")
-//    public ResponseEntity<String> helloWorld(HttpServletRequest req) {
-//        return new ResponseEntity<>("Hello World!", HttpStatus.OK);
-//    }
-//
     @GetMapping(path = "/**")
-    public ResponseEntity<String> helloWorldTest(HttpServletRequest req) {
+    public ResponseEntity<String> helloWorldGet(HttpServletRequest req) {
 
         String url = clarityUrl + req.getRequestURI();
         ResponseEntity<String> resp = new RestTemplate().exchange(url, HttpMethod.GET, new HttpEntity<>(httpHeaders()), String.class, HttpStatus.ACCEPTED);
+
+        System.out.println(req.getRequestURI());
+
+        return resp;
+    }
+
+    @PostMapping(path = "/**")
+    public ResponseEntity<String> helloWorldPost(HttpServletRequest req) {
+
+        String url = clarityUrl + req.getRequestURI();
+        ResponseEntity<String> resp = new RestTemplate().exchange(url, HttpMethod.POST, new HttpEntity<>(httpHeaders()), String.class, HttpStatus.ACCEPTED);
+
+        System.out.println(req.getRequestURI());
 
         return resp;
     }
